b64a3b85f0549471f54fcadefe53a1ae80b77609
ShioriiCodes <kenzunknowntv@gmail.com>
2024-09-13 00:13:55 +0800

    main

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+
+// JavaScript to handle menu toggle
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.nav-menu ul').classList.toggle('show');
+});
